26bf8620f30d85987057589f3f94fbcc726520de
NovicovMykhailo <mikenovicov@gmail.com>
2023-10-20 20:03:53 +0300

    add audio logic

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,14 +1,33 @@
-const audioPlayer = document.querySelector('#audio')
-const audioBtn = document.querySelector('.controls')
+const audioPlayer = document.querySelector('#audio');
+const audioBtn = document.querySelector('.controls');
 
-audioBtn.addEventListener('click', handleClickAudio)
-function handleClickAudio(e){
-    
-    audioBtn.classList.toggle('muted')
-    if(audioBtn.classList.contains('muted')){
-        audioPlayer.pause()
-    }else{
-        audioPlayer.play()
+audioBtn.addEventListener('click', handleClickAudio);
+
+function handleClickAudio(e) {
+
+  if (audioBtn.classList.contains('muted')) {
+    getSoundAndFadeAudio();
+    audioBtn.classList.add('muted')
+
+  } else {
+    audioPlayer.play();
+    audioPlayer.volume=1
+    audioBtn.classList.remove('muted')
+  }
+}
+
+addEventListener('DOMContentLoaded', () => audioPlayer.play());
+
+function getSoundAndFadeAudio() {
+  let fadePoint = 0.2;
+
+  const fadeAudio = setInterval(function () {
+    if (audioPlayer.currentTime >= fadePoint && audioPlayer.volume > 0.1) {
+      console.log('fadeAudio ->  audioPlayer.volume', audioPlayer.volume);
+      audioPlayer.volume -= 0.1;
+    } else {
+      clearInterval(fadeAudio);
+      audioPlayer.pause();
     }
-
+  }, 200);
 }
