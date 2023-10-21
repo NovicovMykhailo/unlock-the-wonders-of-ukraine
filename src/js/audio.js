cdb9af27e874dfc1f2913cb9e09c5b6e7f80811e
NovicovMykhailo <mikenovicov@gmail.com>
2023-10-21 15:55:07 +0300

    optimizing photos

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,28 +1,57 @@
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+
 const audioPlayer = document.querySelector('#audio');
 const audioBtn = document.querySelector('.controls');
 
 audioBtn.addEventListener('click', handleClickAudio);
 
 function handleClickAudio(e) {
-audioBtn.classList.toggle('muted')
+  audioBtn.classList.toggle('muted');
   if (audioBtn.classList.contains('muted')) {
     getSoundAndFadeAudio();
-    
+  }
 
-  } else {
+  if (!audioBtn.classList.contains('muted')) {
     audioPlayer.play();
-    audioPlayer.volume=1
+    audioPlayer.volume = 1;
   }
 }
 
-addEventListener('DOMContentLoaded', () => audioPlayer.play());
+addEventListener('DOMContentLoaded', () => {
+  let startPlayPromise = audioPlayer.play();
+
+  if (startPlayPromise !== undefined) {
+    startPlayPromise
+      .then(() => {
+        audioBtn.classList.remove('muted');
+      })
+      .catch(error => {
+        audioBtn.classList.add('muted');
+        if (error.name === 'NotAllowedError') {
+          console.log('autoplay audio is not aloowed')
+          // Confirm.show(
+          //   'Welcome to Our Site',
+          //   'Do you want to play background atmosphere?',
+          //   'Yes',
+          //   'No',
+          //   () => {
+          //     audioPlayer.play();
+          //     audioBtn.classList.remove('muted');
+          //   },
+          //   () => audioPlayer.pause(),
+
+          //   { position: 'center-top' }
+          // );
+        }
+      });
+  }
+});
 
 function getSoundAndFadeAudio() {
   let fadePoint = 0.2;
 
   const fadeAudio = setInterval(function () {
     if (audioPlayer.currentTime >= fadePoint && audioPlayer.volume > 0.1) {
-      console.log('fadeAudio ->  audioPlayer.volume', audioPlayer.volume);
       audioPlayer.volume -= 0.1;
     } else {
       clearInterval(fadeAudio);
